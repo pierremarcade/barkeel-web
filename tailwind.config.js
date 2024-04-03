672b715e877d87b7ed87b89267150e353c39f1d5
Pierre Marcadé <pierremarcade@gmail.com>
2024-04-03 22:48:51 +0200

    feat: add pagination vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
     },
   },
   plugins: [
-    // ...
+    require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
   ],
 }
