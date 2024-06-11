29171a5214f35f614e318c11cb0b09c659fe042f
Pierre MARCADE <p.marcade@hookto.fr>
2024-06-11 17:53:46 +0200

    chore: upgrade crate version

--- src/app/views/form/form.js
+++ src/app/views/form/form.js
@@ -1,16 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
     changeDateTimeLocalFormat();
     handleSelectAndRadioElements();
+    const Block = Quill.import('blots/block');
+    const Container = Quill.import('blots/container');
+    const Break = Quill.import('blots/break');
+    const TextBlot = Quill.import('blots/text');
+    const Cursor = Quill.import('blots/cursor');
+
+    class CodeBlockContainer extends Container {
+        static create(value) {
+            const domNode = super.create(value) ;
+            domNode.setAttribute('spellcheck', 'false');
+            domNode.setAttribute('class', 'prism-code ql-code-block-container ');
+            return domNode;
+        }
+    }
+    
+    class CodeBlock extends Block {
+        static TAB = '  ';
+        static register() {
+          Quill.register(CodeBlockContainer);
+        }
+    }
+
+    CodeBlockContainer.blotName = 'code-block-container';
+    CodeBlockContainer.tagName = 'pre';
+    CodeBlockContainer.allowedChildren = [CodeBlock];
+
+    CodeBlock.blotName = 'code-block';
+    CodeBlock.className = 'ql-code-block';
+    CodeBlock.tagName = 'DIV';
+    CodeBlock.allowedChildren = [TextBlot, Break, Cursor];
+    CodeBlock.requiredContainer = CodeBlockContainer;
+      
+    Quill.register(CodeBlock);
 
     var editorElements = document.querySelectorAll('.editor');
     editorElements.forEach(function(editorElement) {
         var editorId = editorElement.id;
         var textarea = document.querySelector(`textarea[data-editor-id="${editorId}"]`);
+        textarea.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(titleElement) {
+            var slug = createSlug(titleElement.textContent); // Assurez-vous que cette fonction est définie quelque part
+            titleElement.id = slug;
+        });
         var initialContent = textarea ? textarea.value : '';
         var quill = new Quill(editorElement, {
             theme: 'snow',
             modules: {
-                syntax: true, 
+                syntax: true,
                 toolbar: [
                     ['bold', 'italic', 'underline', 'strike'],
                     ['blockquote', 'code-block'],
@@ -23,15 +60,20 @@ document.addEventListener('DOMContentLoaded', function() {
                     [{ 'color': [] }, { 'background': [] }],
                     [{ 'font': [] }],
                     [{ 'align': [] }],
-                    ['clean']
-                ]
+                    ['clean'],
+                ],
+               
             }
         });
         if (initialContent !== '') {
             quill.root.innerHTML = initialContent;
         }
-        
         quill.on('text-change', function(delta, oldDelta, source) {
+            var root = quill.root
+            root.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(titleElement) {
+                var slug = createSlug(titleElement.textContent); // Assurez-vous que cette fonction est définie quelque part
+                titleElement.id = slug;
+            });
             if (source === 'user') {
                 textarea.value = quill.root.innerHTML;
             }
@@ -44,17 +86,22 @@ function changeDateTimeLocalFormat() {
     const form = document.querySelector('form');
     const datetimeFields = form.querySelectorAll('input[type="datetime-local"]');
     datetimeFields.forEach(function(field) {
-        console.log(field.id)
         var datetime_field = document.querySelector(`input[data-datetime="${field.id}"]`);
         field.value = datetime_field.value;
     });
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        console.log(form)
+        form.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(titleElement) {
+            var slug = createSlug(titleElement.textContent);
+            titleElement.setAttribute('id', slug);
+        });
+
         datetimeFields.forEach(function(field) {
             var datetime_field = document.querySelector(`input[data-datetime="${field.id}"]`);
             let date = new Date(field.value);
             datetime_field.value = date.toISOString().slice(0, 19);
-            console.log(datetime_field.value)
         });
         form.submit();
     });
@@ -132,4 +179,8 @@ function createRadioInput(name, value, labelText, selected) {
                 class="block text-sm font-medium leading-6 text-gray-900">${labelText}</label>
         </div>
     `;
+}
+
+function createSlug(text) {
+    return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
 }
