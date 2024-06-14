e8d751d4a33f32c06cee942786a6ef9ff4075e94
Pierre Marcadé <pierremarcade@gmail.com>
2024-06-14 22:53:57 +0200

    refactor: remove unused file

--- src/app/jobs/mail.rs
+++ src/app/jobs/mail.rs
@@ -5,29 +5,49 @@ use barkeel_derives::DelayedJob;
 use barkeel_lib::mailer::Email;
 use std::error::Error;
 use async_trait::async_trait;
+use serde_json::Map;
 
 #[derive(DelayedJob)]
 pub struct Mail;
 
 #[async_trait]
 impl JobTrait for Mail {
-    async fn perform(&self, _args: Option<Vec<Value>>) -> Result<(), Box<dyn Error>> {
-        let email = Email {
-            from: "sfsdfsdf".to_string(),
-            to: "sdfsdfsdf".to_string(),
-            subject: "sdfdsfdsf".to_string(),
-            body: "sdgfsdfsdfdsf".to_string(),
-        };
-        let mailer = MailerBuilder::new();
-        match mailer.send(email).await{
-            Ok(_) => {
-                println!("Email sent successfully!");
-                Ok(())
-            },
-            Err(e) => {
-                println!("Could not send email: {:?}", e);
-                Err(e)
-            },
+    async fn perform(&self, args: Option<Vec<Value>>) -> Result<(), Box<dyn Error>> {
+        if let Some(args) = args {
+            let mut email = Email {
+                from: String::from("default@example.com"),
+                to: String::from("default@example.com"),
+                subject: String::from("Default Subject"),
+                body: String::from("Default Body"),
+            };
+            for arg in args {
+                let map: Map<String, Value> = serde_json::from_value(arg).expect("Failed to parse argument");
+            
+                for (key, value) in map {
+                    match key.as_str() {
+                        "from" => email.from = value.as_str().unwrap_or("default@example.com").to_string(),
+                        "to" => email.to = value.as_str().unwrap_or("default@example.com").to_string(),
+                        "subject" => email.subject = value.as_str().unwrap_or("Default Subject").to_string(),
+                        "body" => email.body = value.as_str().unwrap_or("Default Body").to_string(),
+                        _ => {}
+                    }
+                }
+            }
+            
+            let mailer = MailerBuilder::new();
+            match mailer.send(email).await{
+                Ok(_) => {
+                    println!("Email sent successfully!");
+                    Ok(())
+                },
+                Err(e) => {
+                    println!("Could not send email: {:?}", e);
+                    Err(e)
+                },
+            }
+        }else {
+            println!("No arguments provided for sending email.");
+            Err("No arguments provided".into())
         }
     }
 }
