f8444a716c90feab2c58f0eb97d0620ec62fb33a
Pierre Marcadé <pierremarcade@gmail.com>
2024-07-22 23:08:47 +0200

    feat: add crud macro rules

--- src/app/controllers/mod.rs
+++ src/app/controllers/mod.rs
@@ -33,21 +33,213 @@ pub fn is_csrf_token_valid(headers: HeaderMap, config: Arc<Config>, csrf_token: 
     }
     return false;
 }
+#[macro_export]
+macro_rules! crud {
+    ($resource:ident, $model:ident, $form:ident) => {
+        index!($resource, $model);
+        new!($resource, $model);
+        edit!($resource, $model);
+        show!($resource, $model);
+        delete!($resource, $model);
+        create!($resource, $model, $form); 
+        update!($resource, $model, $form);
+    };
+}
 
 #[macro_export]
-macro_rules! render_html {
-    ($config:ident, $rendered:ident) => {
-        {
-            match $rendered {
-                Ok(result) => {
-                    Response{status_code: axum::http::StatusCode::OK, content_type: "text/html", datas: result}
+macro_rules! create {
+    ($resource:ident, $model:ident, $form:ident) => {
+        pub async fn create(Extension(mut current_user): Extension<AuthState>, headers: HeaderMap, State(config): State<Arc<Config>>, Form(payload): Form<$form>) -> impl IntoResponse {
+            if is_csrf_token_valid(headers.clone(), config.clone(), payload.clone().csrf_token) {
+                let table_name = stringify!($resource);
+                match payload.validate() {
+                    Ok(_) => {
+                        if let Some(user) = current_user.get_user().await {
+                            let _inserted_record: $model = diesel::insert_into($resource)
+                            .values(insert_values(payload, user.clone()))
+                            .get_result(&mut config.database.pool.get().unwrap())
+                            .expect("Error inserting data");
+                        }
+                        let _ = Redirect::to(format!("/{}", table_name).as_str());
+                        let serialized = serde_json::to_string(&"Menu created").unwrap();
+                        render_json!(StatusCode::OK, serialized)
+                    },
+                    Err(e) => {
+                        let config_ref = config.as_ref();
+                        let form = payload.build_form(config_ref, headers, format!("/{}", table_name).as_str());
+                        render_form!(form, config, current_user, Some(e.clone()))
+                    }
+                }
+            } else {
+                let serialized = serde_json::to_string(&"Invalid CSRF token").unwrap();
+                render_json!(StatusCode::BAD_REQUEST, serialized) 
+            }
+        }
+    }
+}
+
+
+#[macro_export]
+macro_rules! update {
+    ($resource:ident, $model:ident, $form:ident) => {
+        pub async fn update(Extension(mut current_user): Extension<AuthState>, headers: HeaderMap, State(config): State<Arc<Config>>, Path(param_id): Path<i32>, Form(payload): Form<$form>) -> impl IntoResponse {
+            if is_csrf_token_valid(headers.clone(), config.clone(), payload.clone().csrf_token) {
+                let table_name = stringify!($resource);
+                match payload.validate() {
+                    Ok(_) => {
+                        if let Some(user) = current_user.get_user().await {
+                            let _updated_record: $model = diesel::update($resource)
+                                .filter(id.eq(param_id))
+                                .set(update_values(payload, user.clone()))
+                                .get_result(&mut config.database.pool.get().unwrap())
+                                .expect("Error updating data");
+                        }
+                            let _ = Redirect::to(format!("/{}", table_name).as_str());
+                        let serialized = serde_json::to_string(&"Menu updated").unwrap();
+                        render_json!(StatusCode::OK, serialized)
+                    },
+                    Err(e) => {
+                        let config_ref = config.as_ref();
+                        let form = payload.build_form(config_ref, headers, format!("/{}", table_name).as_str());
+                        render_form!(form, config, current_user, Some(e.clone()))
+                    }
+                }
+            } else {
+                let serialized = serde_json::to_string(&"Invaid CSRF token").unwrap();
+                render_json!(StatusCode::BAD_REQUEST, serialized) 
+            }
+        }
+    }
+}
+
+
+#[macro_export]
+macro_rules! index {
+    ($resource:ident, $model:ident) => {
+        pub async fn index(Extension(current_user): Extension<AuthState>, Query(pagination_query): Query<PaginationQuery>, headers: HeaderMap, State(config): State<Arc<Config>>) -> impl IntoResponse {
+            let total_results: i64 = get_total!(config, $resource);
+            let pagination = Pagination::new(pagination_query, total_results);
+            match $resource.limit(pagination.per_page as i64).offset(pagination.offset as i64).load::< $model>(&mut config.database.pool.get().unwrap()) {
+                Ok(results) => {
+                    if get_content_type(headers) == "application/json" {
+                        render_json!(StatusCode::OK, results)
+                    } else {
+                        let table_name = stringify!($resource);
+                        let model_name = table_name.to_singular();
+                        let model_class = table_name.to_class_case();
+                        let mut context = prepare_tera_context(current_user).await;
+                        context.insert("title", &model_class.as_str());
+                        context.insert("base_url", format!("/{}", table_name).as_str());
+                        context.insert("description", format!("A list of all the {}.", table_name).as_str());
+                        context.insert("datas", &results);
+                        context.insert("total_pages", &pagination.total_pages);
+                        context.insert("current_page", &pagination.current_page);
+                        context.insert("current_page_string", &pagination.current_page.to_string());
+                        context.insert("offset", &pagination.offset);
+                        context.insert("per_page", &pagination.per_page);
+                        context.insert("page_numbers", &pagination.generate_page_numbers());
+                        let tera: &mut tera::Tera = &mut config.template.clone();
+                        let custom_index_name = format!("{}/index.html", model_name);
+                        let mut custom_index_path = env::current_dir().expect("REASON");
+                        custom_index_path.push(format!("src/app/views/{}/index.html", model_name).as_str());
+                        let filename = if custom_index_path.exists() {
+                            let custom_file_content = fs::read_to_string(custom_index_path).expect("Failed to read the file");
+                            let _ = tera.add_raw_template(custom_index_name.clone().as_str(), &custom_file_content.as_str());
+                            custom_index_name.as_str()
+                        } else {
+                            let _ = tera.add_raw_template("crud/index.html", include_str!("../views/crud/index.html"));
+                            "crud/index.html"
+                        };
+                        
+                        let rendered = tera.render(filename, &context);
+                        render_html!(config, rendered)
+                    }
                 },
                 Err(err) => {
-                    error_controller::handler_error($config, axum::http::StatusCode::BAD_REQUEST, err.to_string())
+                    error_controller::handler_error(config, StatusCode::BAD_REQUEST, err.to_string())
                 }
             }
-        }  
-    };
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! show {
+    ($resource:ident, $model:ident) => {
+        pub async fn show(Extension(current_user): Extension<AuthState>, Path(param_id): Path<i32>, State(config): State<Arc<Config>>) -> impl IntoResponse {
+            let tera: &Tera = &config.template;
+            let mut tera = tera.clone();
+            let table_name = stringify!($resource);
+            let model_name = table_name.to_singular();
+            let model_class = table_name.to_class_case();
+            match $resource.find(param_id).first::<$model>(&mut config.database.pool.get().unwrap()) {
+                Ok(result) => {
+                    let custom_show_name = format!("{}/show.html", model_name);
+                    let mut custom_show_path = env::current_dir().expect("REASON");
+                    custom_show_path.push(format!("src/app/views/{}/show.html", model_name).as_str());
+                    let filename = if custom_show_path.exists() {
+                        let custom_file_content = fs::read_to_string(custom_show_path).expect("Failed to read the file");
+                        let _ = tera.add_raw_template(custom_show_name.clone().as_str(), &custom_file_content.as_str());
+                        custom_show_name.as_str()
+                    } else {
+                        let _ = tera.add_raw_template("crud/show.html", include_str!("../views/crud/show.html"));
+                        "crud/show.html"
+                    };
+                    let mut context = prepare_tera_context(current_user).await;
+                    context.insert("data", &result);
+                    context.insert("title", &model_class.as_str());
+                    context.insert("description", format!("{}'s Detail", model_class).as_str());
+                    let rendered = tera.render(filename, &context).unwrap();
+                    Response{status_code: StatusCode::OK, content_type: "text/html", datas: rendered}
+                },
+                _ => {
+                    error_controller::render_404(config)
+                }
+            }
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! new {
+    ($resource:ident, $model:ident) => {
+        pub async fn new(Extension(current_user): Extension<AuthState>, headers: HeaderMap, State(config): State<Arc<Config>>) -> impl IntoResponse {
+            let config_ref = config.as_ref();
+            let table_name = stringify!($resource);
+            let form = $model::build_create_form(config_ref, headers, format!("/{}", table_name).as_str());
+            render_form!(form, config, current_user, None::<Option<ValidationErrors>>)
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! edit {
+    ($resource:ident, $model:ident) => {
+        pub async fn edit(Extension(current_user): Extension<AuthState>, headers: HeaderMap, Path(param_id): Path<i32>, State(config): State<Arc<Config>>) -> impl IntoResponse {
+            let result = $resource
+                .find(param_id)
+                .first::<$model>(&mut config.database.pool.get().unwrap())
+                .expect("Error loading data");
+            let table_name = stringify!($resource);
+            let config_ref = config.as_ref();
+            let form = result.build_edit_form(config_ref, headers, format!("/{}/{}", table_name, param_id).as_str());
+            render_form!(form, config, current_user, None::<Option<ValidationErrors>>)
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! delete {
+    ($resource:ident, $model:ident) => {
+        pub async fn delete(Path(param_id): Path<i32>, State(config): State<Arc<Config>>) -> Redirect {
+            let table_name = stringify!($resource);
+            diesel::delete($resource)
+                .filter(id.eq(param_id))
+                .execute(&mut config.database.pool.get().unwrap())
+                .expect("Error deleting data");
+            Redirect::to(format!("/{}", table_name).as_str()) 
+        }
+    }
 }
 
 #[macro_export]
@@ -68,15 +260,31 @@ macro_rules! render_form {
 }
 
 #[macro_export]
+macro_rules! render_html {
+    ($config:ident, $rendered:ident) => {
+        {
+            match $rendered {
+                Ok(result) => {
+                    Response{status_code: axum::http::StatusCode::OK, content_type: "text/html", datas: result}
+                },
+                Err(err) => {
+                    error_controller::handler_error($config, axum::http::StatusCode::BAD_REQUEST, err.to_string())
+                }
+            }
+        }  
+    };
+}
+
+#[macro_export]
 macro_rules! render_json {
-    ($config:ident, $results:ident) => {
+    ($status_code:expr, $results:ident) => {
         {
             match serde_json::to_string(&$results) {
                 Ok(serialized) => {
-                    return Response{status_code: axum::http::StatusCode::OK, content_type: "application/json", datas: serialized};
+                    Response{status_code: $status_code, content_type: "application/json", datas: serialized}
                 },
                 Err(err) => {
-                    return error_controller::handler_error($config, axum::http::StatusCode::BAD_REQUEST, err.to_string());
+                    Response{status_code: axum::http::StatusCode::BAD_REQUEST, content_type: "application/json", datas: err.to_string()}
                 }
             }
         }  
